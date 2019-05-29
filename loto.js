0123deebb407c468aad5b2a28bd4305e82475279
SinisaK <42300134+SinisaK@users.noreply.github.com>
2019-05-29 19:08:02 +0200

    Add files via upload

---
+++ loto.js
@@ -0,0 +1,873 @@
+alert("KLIKNI NA BROJ I SAZNAJ KOLIKO JE PUTA BIO IZVUCEN");
+
+var a=[2,4,10,11,13,19,35,5,9,10,22,23,27,35,1,3,9,24,27,31,33,1,13,14,20,21,27,36,9,11,18,23,24,31,32,4,14,22,33,36,37,38,6,13,20,21,22,27,36,4,9,13,14,22,26,37,14,15,16,20,22,25,36,9,11,12,16,17,35,37,2,11,16,21,28,33,37,4,5,11,17,28,30,34,4,6,9,13,20,34,37];
+
+
+function jedan(){
+    
+    let b=1;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+if(brojac==1){
+    document.calc.res.value="broj 1 je izvucen jednom";
+}
+    if(brojac<1){
+        document.calc.res.value="broj 1 nije izvucen";
+    }
+if(brojac>1){
+    document.calc.res.value="broj 1 je izvucen:"+brojac+" "+"puta";
+
+}
+}
+
+function dva(){
+    
+    let b=2;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+    if(brojac==1){
+        document.calc.res.value="broj 2 je izvucen jednom";
+    }
+    
+    
+        if(brojac<1){
+            document.calc.res.value="broj 2 nije izvucen";
+        }
+    if(brojac>1){
+    
+    
+    
+    
+    
+        document.calc.res.value="broj 2 je izvucen:"+brojac+" "+"puta";
+    }
+}
+function tri(){
+    
+    let b=3;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 3 je izvucen jednom";
+    }
+    
+    
+        if(brojac<1){
+            document.calc.res.value="broj 3 nije izvucen ";
+        }
+    if(brojac>1){
+    
+    
+    
+    
+    
+        document.calc.res.value="broj 3 je izvucen:"+brojac+" "+"puta";
+}
+}
+
+function cetiri(){
+    
+    let b=4;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 4 je izvucen jednom";
+    }
+    
+    
+        if(brojac<1){
+            document.calc.res.value="broj 4 nije izvucen ";
+        }
+    if(brojac>1){
+    
+    
+    
+    
+    
+        document.calc.res.value="broj 4 je izvucen:"+brojac+" "+"puta";
+}
+}
+
+function pet(){
+    
+    let b=5;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 5 je izvucen jednom";
+    }
+    
+    
+        if(brojac<1){
+            document.calc.res.value="broj 5 nije izvucen ";
+        }
+    if(brojac>1){
+    
+    
+    
+    
+    
+        document.calc.res.value="broj 5 je izvucen:"+brojac+" "+"puta";
+}
+}
+
+function sest(){
+    
+    let b=6;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 6 je izvucen jednom";
+    }
+    
+    
+        if(brojac<1){
+            document.calc.res.value="broj 6 nije izvucen ";
+        }
+    if(brojac>1){
+    
+    
+    
+    
+    
+        document.calc.res.value="broj 6 je izvucen:"+brojac+" "+"puta";
+}
+}
+function sedam(){
+    
+    let b=7;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 7 je izvucen jednom";
+    }
+    
+    
+        if(brojac<1){
+            document.calc.res.value="broj 7 nije izvucen ";
+        }
+    if(brojac>1){
+    
+    
+    
+    
+    
+        document.calc.res.value="broj 7 je izvucen:"+brojac+" "+"puta";
+}
+}
+function osam(){
+    
+    let b=8;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 8 je izvucen jednom";
+    }
+    
+    
+        if(brojac<1){
+            document.calc.res.value="broj 8 nije izvucen ";
+        }
+    if(brojac>1){
+    
+    
+    
+    
+    
+        document.calc.res.value="broj 8 je izvucen:"+brojac+" "+"puta";
+}
+}
+function devet(){
+    
+    let b=9;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 9 je izvucen jednom";
+    }
+    
+    
+        if(brojac<1){
+            document.calc.res.value="broj 9 nije izvucen ";
+        }
+    if(brojac>1){
+    
+    
+    
+    
+    
+        document.calc.res.value="broj 9 je izvucen:"+brojac+" "+"puta";
+}
+}
+function deset(){
+    
+    let b=10;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 10 je izvucen jednom";
+    }
+    
+    
+        if(brojac<1){
+            document.calc.res.value="broj 10 nije izvucen ";
+        }
+    if(brojac>1){
+    
+    
+    
+    
+    
+        document.calc.res.value="broj 10 je izvucen:"+brojac+" "+"puta";
+}
+}
+function jedanaest(){
+    
+    let b=11;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 11 je izvucen jednom";
+    }
+    
+    
+        if(brojac<1){
+            document.calc.res.value="broj 11 nije izvucen ";
+        }
+    if(brojac>1){
+    
+    
+    
+    
+    
+        document.calc.res.value="broj 11 je izvucen:"+brojac+" "+"puta";
+}
+}
+function dvanaest(){
+    
+    let b=12;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 12 je izvucen jednom";
+    }
+    
+    
+        if(brojac<1){
+            document.calc.res.value="broj 12 nije izvucen ";
+        }
+    if(brojac>1){
+    
+    
+    
+    
+    
+        document.calc.res.value="broj 12 je izvucen:"+brojac+" "+"puta";
+}
+}
+function trinaest(){
+    
+    let b=13;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 13 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 13 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 13 je izvucen:"+brojac+" "+"puta";
+}
+}
+function cetrnaest(){
+    
+    let b=14;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 14 je izvucen jednom";
+    }
+    
+    
+        if(brojac<1){
+            document.calc.res.value="broj 14 nije izvucen ";
+        }
+    if(brojac>1){
+    
+    
+    
+    
+    
+        document.calc.res.value="broj 14 je izvucen:"+brojac+" "+"puta";
+}
+}
+function petnaest(){
+    
+    let b=15;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 15 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 15 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 15 je izvucen:"+brojac+" "+"puta";
+}
+}
+function sesnaest(){
+    
+    let b=16;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 16 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 16 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 16 je izvucen:"+brojac+" "+"puta";
+}
+}
+function sedamnaest(){
+    
+    let b=17;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 17 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 17 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 17 je izvucen:"+brojac+" "+"puta";
+}
+}
+function osamnaest(){
+    
+    let b=18;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 18 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 18 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 18 je izvucen:"+brojac+" "+"puta";
+}
+}
+function devetnaest(){
+    
+    let b=19;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 19 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 19 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 19 je izvucen:"+brojac+" "+"puta";
+}
+}
+function dvadeset(){
+    
+    let b=20;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 20 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 20 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 20 je izvucen:"+brojac+" "+"puta";
+}
+}
+function dvadesetjedan(){
+    
+    let b=21;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 21 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 21 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 21 je izvucen:"+brojac+" "+"puta";
+}
+}
+function dvadesetdva(){
+    
+    let b=22;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 22 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 22 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 22 je izvucen:"+brojac+" "+"puta";
+}
+}
+function dvadesettri(){
+    
+    let b=23;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 23 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 23 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 23 je izvucen:"+brojac+" "+"puta";
+}
+}
+function dvadesetcetiri(){
+    
+    let b=24;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 24 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 24 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 24 je izvucen:"+brojac+" "+"puta";
+}
+}
+function dvadesetpet(){
+    
+    let b=25;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 25 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 25 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 25 je izvucen:"+brojac+" "+"puta";
+}
+}
+function dvadesetsest(){
+    
+    let b=26;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 26 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 26 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 26 je izvucen:"+brojac+" "+"puta";
+}
+}
+function dvadesetsedam(){
+    
+    let b=27;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 27 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 27 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 27 je izvucen:"+brojac+" "+"puta";
+}
+}
+function dvadesetosam(){
+    
+    let b=28;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 28 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 28 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 28 je izvucen:"+brojac+" "+"puta";
+}
+}
+function dvadesetdevet(){
+    
+    let b=29;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 29 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 29 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 29 je izvucen:"+brojac+" "+"puta";
+}
+}
+function trideset(){
+    
+    let b=30;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 30 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 30 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 30 je izvucen:"+brojac+" "+"puta";
+}
+}
+function tridesetjedan(){
+    
+    let b=31;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 31 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 31 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 31 je izvucen:"+brojac+" "+"puta";
+}
+}
+function tridesetdva(){
+    
+    let b=32;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 32 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 32 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 32 je izvucen:"+brojac+" "+"puta";
+}
+}
+function tridesettri(){
+    
+    let b=33;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 33 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 33 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 33 je izvucen:"+brojac+" "+"puta";
+}
+}
+function tridesetcetiri(){
+    
+    let b=34;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 34 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 34 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 34 je izvucen:"+brojac+" "+"puta";
+}
+}
+function tridesetpet(){
+    
+    let b=35;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 35 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 35 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 35 je izvucen:"+brojac+" "+"puta";
+}
+}
+function tridesetsest(){
+    
+    let b=36;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 36 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 36 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 36 je izvucen:"+brojac+" "+"puta";
+}
+}
+function tridesetsedam(){
+    
+    let b=37;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 37 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 37 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 37 je izvucen:"+brojac+" "+"puta";
+}
+}
+function tridesetosam(){
+    
+    let b=38;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 38 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 38 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 38 je izvucen:"+brojac+" "+"puta";
+}
+}
+function tridesetdevet(){
+    
+    let b=39;
+    let brojac=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b){
+            brojac++;
+        }
+    }
+  
+    if(brojac==1){
+        document.calc.res.value="broj 39 je izvucen jednom";
+    }
+        if(brojac<1){
+            document.calc.res.value="broj 39 nije izvucen ";
+        }
+    if(brojac>1){
+        document.calc.res.value="broj 39 je izvucen:"+brojac+" "+"puta";
+}
+}
